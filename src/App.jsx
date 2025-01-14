eb4b9312c726497a6c48fdfd76e5486f4a7dc284
WebackInner <abdallah@weback.in>
2025-01-14 16:12:40 +0100

    Initial commit from your app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,41 @@
-import React from 'react';
-  import './App.css';
+import React, { useState } from 'react';
 
-  function App() {
-    return (
-      <div className="App">
-        <h1>Hello, World!</h1>
-      </div>
-    );
-  }
+    function App() {
+      const [todos, setTodos] = useState([]);
+      const [newTodo, setNewTodo] = useState('');
 
-  export default App;
+      const addTodo = () => {
+        if (newTodo.trim() !== '') {
+          setTodos([...todos, { id: Date.now(), text: newTodo }]);
+          setNewTodo('');
+        }
+      };
+
+      const deleteTodo = (id) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+      };
+
+      return (
+        <div className="todo-list">
+          <h2>Todo List</h2>
+          <div className="todo-input-container">
+            <input
+              type="text"
+              className="todo-input"
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+              placeholder="Add new todo"
+            />
+            <button className="add-button" onClick={addTodo}>Add</button>
+          </div>
+          {todos.map(todo => (
+            <div key={todo.id} className="todo-item">
+              <span>{todo.text}</span>
+              <button className="delete-button" onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    export default App;
